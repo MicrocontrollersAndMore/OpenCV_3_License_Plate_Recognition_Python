e1548ee5112a24cd4904ce324feb0b3600bafe23
MicrocontrollersAndMore <cdahms123@gmail.com>
2015-12-20 09:46:14 -0500

    1

--- GlobalCounters.py
+++
@@ -1,6 +0,0 @@
-# GlobalCounters.py
-
-# module variables ################################################################################
-intNumTimesInCheckIfValid = 0
-intNumTimesPastFirstIf = 0
-intNumTimesPastWidthAndHeight = 0
